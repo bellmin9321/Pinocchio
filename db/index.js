6899878ee492fbe91b82d1ba7d9fa55a57c5e62a
JONGMIN KIM <bellmin9321@gmail.com>
2022-07-12 06:35:48 +0900

    Add: mongoDB 연결 & custom 에러 생성

---
+++ db/index.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const { ServerError } = require("../error/customError");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected...");
+  } catch (error) {
+    console.log("Failed to conect to MongoDB");
+
+    return next(new ServerError());
+  }
+};
+
+module.exports = connectDB;
